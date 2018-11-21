f4a2b3141c4600a313dbc8921af65395ba7cbb9c
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2018-11-20 20:37:04 -0500

    Back to the proper recipes.

--- ngram.py
+++ ngram.py
@@ -11,21 +11,24 @@ from itertools import tee
 
 # CONSUMER
 def consume(iterator, n):
-    "Advance the iterator n-steps ahead. If n is none, consume entirely."
+    """
+    Advance the iterator n-steps ahead. If n is none, consume entirely.
+    From: https://docs.python.org/2/library/itertools.html#recipes
+    """
     # Use functions that consume iterators at C speed.
     if n is None:
         # feed the entire iterator into a zero-length deque
-        iterator = deque(iterator, maxlen=0)
+        deque(iterator, maxlen=0)
     else:
         # advance to the empty slice starting at position n
-        iterator = next(islice(iterator, n, n), None)
+        next(islice(iterator, n, n), None)
     return iterator
 
 
 
 def consume3(iterator, n):
     '''Advance the iterator n-steps ahead. If n is none, consume entirely.'''
-    iterator = deque(islice(iterator, n), maxlen=0)
+    deque(islice(iterator, n), maxlen=0)
     return iterator
 
 
@@ -46,9 +49,8 @@ def ngram_consume(iterable, n=2):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
     assert n > 0, 'Cannot create negative n-grams.'
     #return izip(*[consume(s, i) for i, s in enumerate(tee(iterable, n))])
-    return izip([consume(s, i) for i, s in enumerate(tee(iterable, n))])
-    #context = [consume(s, i) for i, s in enumerate(tee(iterable, n))]
-    #return izip(*context)
+    context = [consume(s, i) for i, s in enumerate(tee(iterable, n))]
+    return izip(*context)
 
 
 
